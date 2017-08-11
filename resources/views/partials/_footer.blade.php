d9ec10f29998c850507955e0bf19691f5d854d70
07-12 <07-12>
2017-08-11 15:34:51 -0300

    Footer(algumas coisas)

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,3 +1,8 @@
-      <hr>
 
-      <p class="text-center">Copyright QR Security - Todos os direitos reservados</p>
+ <div class="footer">
+       </br>
+            <hr>
+            ©Copyright QR Security - Todos os direitos reservados
+            <a href="https://designersbrasileiros.com.br/sites-para-quem-trabalha-com-design-grafico/"></a>
+            <img src="../images/facebook.png" alt="facebook" width="25"></img>
+      </div> 

41adb7dbfa825170449b94df734956c4e37bb005
Lucas Manera <caiogmelo@gmail.com>
2017-08-14 21:26:15 -0300

    Alguns erros no hover (preciso de ajudar)

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -2,7 +2,14 @@
  <div class="footer">
        </br>
             <hr>
-            ©Copyright QR Security - Todos os direitos reservados
-            <a href="https://designersbrasileiros.com.br/sites-para-quem-trabalha-com-design-grafico/"></a>
-            <img src="../images/facebook.png" alt="facebook" width="25"></img>
+<div class="figure">
+  <a class="face" href="https://designersbrasileiros.com.br/sites-para-quem-trabalha-com-design-grafico/"> <img class="face" src="../images/facebook.png" alt="Facebook" width="25"></img></a>
+ </div>
+ <div class="figurem">
+  <a class="tile2" href="https://designersbrasileiros.com.br/sites-para-quem-trabalha-com-design-grafico/"> <img class="tile2" src="../images/tw.png" alt="Twitter" width="25"></img></a>
+ </div>
+ <div class="figurer">
+  <a class="tile3" href="https://designersbrasileiros.com.br/sites-para-quem-trabalha-com-design-grafico/"> <img class="tile3" src="../images/instagram_icon.png" alt="Instagram" width="25"></img></a>
+</div>
+©Copyright QR Security - Todos os direitos reservados
       </div> 

8a9e67a1d3190dc2cc0266932172aaa0b4f68976
Matheus Oliveira <matheus.oliveira429@etec.sp.gov.br>
2017-09-22 15:28:45 -0300

    Páginas de login consertadas, bootstrap restaurado

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -3,19 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-4 col-md-offset-4">
+        <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Login do administrador</div>
-                  </br>
+                <div class="panel-heading">Login do Administrador</div>
+
                 <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('admin.login.submit') }}">
-                        {{ csrf_field() }}
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('admin.login.submit') }}">                        {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label"></label>
+                            <label for="email" class="col-md-4 control-label">E-Mail</label>
 
                             <div class="col-md-6">
-                                <input  placeholder="Endereço de Email" id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -26,10 +25,10 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label"></label>
+                            <label for="password" class="col-md-4 control-label">Senha</label>
 
                             <div class="col-md-6">
-                                <input placeholder="*******" id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -39,18 +38,26 @@
                             </div>
                         </div>
 
-                          <div class="form-group">
-                            <div class="col-md-8 col-md-offset-0">
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Lembre-se de Mim
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
-                                    </div>
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-2">
+
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Esqueceu sua senha?
+                                    Esqueceu a senha?
                                 </a>
-                            </div> 
+                            </div>
                         </div>
                     </form>
                 </div>

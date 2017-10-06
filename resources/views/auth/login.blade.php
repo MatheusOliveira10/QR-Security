2c599585b634aa58d2e26321114746f6eb5d343e
Matheus Oliveira <matheus.oliveira429@etec.sp.gov.br>
2017-10-05 23:29:22 -0300

    Conserto das cagadas do Caio

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,6 @@
 @extends('main')
 
 @section('content')
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
